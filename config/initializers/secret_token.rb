7fa65dbefca065f05c595b6ae6d7608f4b9a52a4
Chad Jemmett <chad.jemmett@gmail.com>
2014-01-16 06:15:08 -0700

    first commit. rspec working. boostrap gem added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BeatTime::Application.config.secret_key_base = '1ee3428aceab5bfe576b37e4b4ea8f58b1d93629ad070a6fde47c5652a42138000d9e097c9ca877213516d7ab3a1fa727a45ccdd8b6d8936a03be3b236a87395'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BeatTime::Application.config.session_store :cookie_store, key: '_beat_time_session'
